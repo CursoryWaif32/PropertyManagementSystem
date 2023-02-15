26830daa266ca0bb7b3d983755651f188d95ea2f
Liam Quick <quick.lilalm2@gmail.com>
2023-02-15 09:23:06 +0200

    Refactored Controller class into a controller package and renamed to ApartmentController

--- src/main/java/org/example/controllers/ApartmentController.java
+++ src/main/java/org/example/controllers/ApartmentController.java
@@ -1,29 +1,32 @@
-package org.example;
+package org.example.controllers;
 
+import org.example.ApartmentRepository;
 import org.example.entities.Apartment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
 @RestController
-public class Controller {
+@RequestMapping("/apartments")
+public class ApartmentController {
 
     final
     ApartmentRepository apartmentRepo;
 
-    public Controller(ApartmentRepository apartmentRepo) {
+    public ApartmentController(ApartmentRepository apartmentRepo) {
         this.apartmentRepo = apartmentRepo;
     }
 
 
-    @GetMapping("/apartments/{id}")
+    @GetMapping("/{id}")
     public Optional<Apartment> getApartmentsByID(@PathVariable Long id) {
         return apartmentRepo.findByApartmentID(id);
     }
 
-    @GetMapping("/apartments")
+    @GetMapping
     public Iterable<Apartment> getAllApartments(){
         return apartmentRepo.findAll();
     }
